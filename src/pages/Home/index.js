a7342292dec2337e7efe4b537849f92e8ff35e38
ssemugenyi <ssemugenyiisaac2@gmail.com>
2024-10-18 00:38:29 +0300

    added links to home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </div>
+  );
 };
 
 export default Home;
