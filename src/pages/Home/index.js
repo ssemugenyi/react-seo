4c6b05b795c0fd37f69f083a0a7b82cb07e20473
ssemugenyi <ssemugenyiisaac2@gmail.com>
2024-10-18 01:07:37 +0300

    added image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
+import testImage from "../../assets/images/ankole-4636125_1280.jpg";
 
 const Home = () => {
   return (
     <>
-      <Helmet>
+      <Helmet prioritizeSeoTags>
         <title>About</title>
-        <meta name="description" content="About page" />
-        <meta name="keywords" content="about, page" />
+
         <meta name="og:title" property="og:title" content="About" />
         <meta
           name="og:description"
@@ -23,13 +23,11 @@ const Home = () => {
           content="React Starter"
         />
         <meta name="og:type" property="og:type" content="website" />
-        <meta
-          name="og:url"
-          property="og:url"
-          content="https://reactstarter.com/about"
-        />
-        <meta name="og:image" property="og:image" content={Image} />
+        <meta name="og:url" property="og:url" content="/about" />
+        <meta name="og:image" property="og:image" content={testImage} />
         <link rel="canonical" href="/about" />
+        <meta name="description" content="About page" />
+        <meta name="keywords" content="about, page" />
       </Helmet>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
